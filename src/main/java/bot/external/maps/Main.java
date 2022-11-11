e2d3c810372567df23e5a3d4c0051d67b2bb0d2d
Brat-vseznamus <fvixnin@gmail.com>
2022-11-11 23:21:19 +0300

    finally

--- src/main/java/bot/external/maps/Main.java
+++ src/main/java/bot/external/maps/Main.java
@@ -1,12 +1,15 @@
 package bot.external.maps;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         MapService service = new MapService();
-        service.setText("кафе");
+        service.setText("cafe");
 
         String response = service.sendRequest();
 
-        System.out.println(response);
+        System.out.println(new ObjectMapper().readValue(response, MapResponse.class));
     }
 }
--- src/main/java/bot/external/maps/MapResponse.java
+++ src/main/java/bot/external/maps/MapResponse.java
@@ -1,35 +1,144 @@
 package bot.external.maps;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
+
 import java.util.List;
 
+@ToString
 public class MapResponse {
-    String type;
-    Properties properties;
-    List<Feature> features;
+    public String type;
+    public Properties properties;
+    public List<Feature> features;
 
+    @ToString
     public static class Properties {
 
-        ResponseMetaData responseMetaData;
+        @JsonProperty("ResponseMetaData")
+        public ResponseMetaData responseMetaData;
 
+        @ToString
         public static class ResponseMetaData {
 
-            SearchResponse searchResponse;
-            SearchRequest searchRequest;
+            @JsonProperty("SearchResponse")
+            public SearchResponse searchResponse;
 
+            @JsonProperty("SearchRequest")
+            public SearchRequest searchRequest;
+
+            @ToString
             public static class SearchResponse {
-                Integer found;
-
+                public Integer found;
+                public String display;
+                public List<List<Double>> boundedBy;
             }
-            public static class SearchRequest {}
+            @ToString
+            public static class SearchRequest {
+                public String request;
+                public Integer skip;
+                public Integer results;
+                public List<List<Double>> boundedBy;
+            }
         }
     }
 
+    @ToString
     public static class Feature {
-        String type;
-        Geometry geometry;
-        Properties properties;
+        public String type;
+        public Geometry geometry;
+        public Properties properties;
 
-        public static class Geometry {}
-        public static class Properties {}
+        @ToString
+        public static class Geometry {
+            public String type;
+            public List<Double> coordinates;
+        }
+
+        @ToString
+        public static class Properties {
+            public String name;
+            public String description;
+            public List<List<Double>> boundedBy;
+
+            @JsonProperty("CompanyMetaData")
+            public CompanyMetaData companyMetaData;
+
+            @ToString
+            public static class CompanyMetaData {
+                public String id;
+                public String name;
+                public String address;
+                public String url;
+
+                @JsonProperty("Phones")
+                public List<Phone> phones;
+
+                @JsonProperty("Categories")
+                public List<Category> categories;
+
+                @JsonProperty("Hours")
+                public Hours hours;
+
+                @ToString
+                public static class Phone {
+                    public String type;
+                    public String formatted;
+                }
+
+                @ToString
+                public static class Category {
+
+                    @JsonProperty("class")
+                    public String className;
+
+                    public String name;
+                }
+
+                @ToString
+                public static class Hours {
+                    public String text;
+
+                    @JsonProperty("Availabilities")
+                    public List<Availability> availabilities;
+
+                    @ToString
+                    public static class Availability {
+
+                        @JsonProperty("Intervals")
+                        public List<Interval> intervals;
+
+                        @JsonProperty("Everyday")
+                        public Boolean everyDay = false;
+
+                        @JsonProperty("Monday")
+                        public Boolean monday = false;
+
+                        @JsonProperty("Tuesday")
+                        public Boolean tuesday = false;
+
+                        @JsonProperty("Wednesday")
+                        public Boolean wednesday = false;
+
+                        @JsonProperty("Thursday")
+                        public Boolean thursday = false;
+
+                        @JsonProperty("Friday")
+                        public Boolean friday = false;
+
+                        @JsonProperty("Saturday")
+                        public Boolean saturday = false;
+
+                        @JsonProperty("Sunday")
+                        public Boolean sunday = false;
+
+                        @ToString
+                        public static class Interval {
+                            public String to;
+                            public String from;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
