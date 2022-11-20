ed33393d6dc78f1079bf3e54c65496a950523cf5
mk17ru <mk17.ru@mail.ru>
2022-11-20 13:24:21 +0300

    add categories

--- src/main/java/bot/backend/nodes/categories/Category.java
+++ src/main/java/bot/backend/nodes/categories/Category.java
@@ -1,5 +1,15 @@
 package bot.backend.nodes.categories;
 
 public enum Category {
-    FOOD, ACTIVE, CULTURE, SETTINGS, DEFAULT;
+    FOOD, CULTURE, ACTIVE, EXTREME, MASS_EVENTS, PARTY, SETTINGS, HEALTH, DEFAULT
 }
+
+
+// FOOD -> Фастфуд	Японская	Азиатская	Кавказская	Европейская
+// CULTURE -> movie, theatre, musical
+// ACTIVE -> football, basketball спорт с мужиками "во дворе"
+// EXTREME -> Skydiving, Road jumping - Это жестко
+// MASS_EVENTS -> Football match, Basketball, Festival, Ярмарки,
+// PARTY -> тусовки, клубы, бары(не в плане еды)
+// SETTINGS -> сколько стоит, день
+// HEALTH -> SPA
