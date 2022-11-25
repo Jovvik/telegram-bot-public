ab03904b164ea95f891ef34af712f21f9f655b07
mk17ru <mk17.ru@mail.ru>
2022-11-25 19:34:44 +0300

    A lot of changes for DB constructors

---
+++ src/main/java/bot/external/kudago/KudaGoConverter.kt
@@ -0,0 +1,19 @@
+package bot.external.kudago
+
+import bot.backend.nodes.location.Location
+import org.json.JSONObject
+
+class KudaGoConverter {
+
+    val genres : List<String> = listOf(
+        "drama", "comedy", "musical", "adventure", "thriller", "horror", "crime", "fantasy", "documentary",
+    );
+
+//    fun convertToLocation(location: JSONObject?): Location {
+////        return Location(
+////            name=location.get("title"),
+////            tags=Set.of(),
+////
+////        );
+//    }
+}
--- src/main/java/bot/external/kudago/KudaGoServer.java
+++ src/main/java/bot/external/kudago/KudaGoServer.java
@@ -1,5 +1,7 @@
 package bot.external.kudago;
 
+import bot.backend.nodes.location.Location;
+import bot.entities.LocationEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -16,36 +18,52 @@ import java.util.List;
 
 public class KudaGoServer {
 
-    private static final String API_URL = "https://kudago.com/public-api/v1.2/place-categories/?lang=ru";
+    private KudaGoConverter kudaGoConverter;
 
-
-    public List<String> getPlaces() throws IOException {
-
+    public List<String> getAllCategories() throws IOException {
+        final String API_URL = "https://kudago.com/public-api/v1.2/place-categories/?lang=ru";
         URL obj = new URL(API_URL);
-
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
-
         String inputLine;
-
         StringBuffer response = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
-
         JSONArray myResponse = new JSONArray(response.toString());
-
-
-
         for (int i = 0; i < myResponse.length(); ++i) {
             JSONObject current = myResponse.getJSONObject(i);
             System.out.println(current.get("slug"));
         }
-
         return null;
     }
+
+    //https://kudago.com/public-api/v1.4/places/?page_size=20&fields=id,title,address,location,phone,site_url,coords,timetable,categories&location=spb&categories=theatre
+
+    //https://kudago.com/public-api/v1.2/movies/?fields=status,slug
+
+
+
+    public List<Location> getMovieByGenres(List<String> genres) throws IOException {
+        final String API_URL = "https://kudago.com/public-api/v1.2/movies/?location=spb&fields=id,title,address,location,phone,site_url,coords,timetable,genres&page_size=10";
+        URL obj = new URL(API_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        JSONObject jsonObject = new JSONObject(response.toString());
+        JSONArray myResponse = jsonObject.getJSONArray("results");
+        for (int i = 0; i < myResponse.length(); ++i) {
+            JSONObject current = myResponse.getJSONObject(i);
+            kudaGoConverter.convertToLocation(current);
+        }
+        return null;
+    }
+
 
 }
