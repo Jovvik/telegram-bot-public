014310d39a48540276aadd0e45dcec9b866d8c36
ST-1580 <steep1580@yandex.ru>
2022-11-27 15:35:49 +0300

    Changed Query

--- src/main/java/bot/repositories/LocationRepository.java
+++ src/main/java/bot/repositories/LocationRepository.java
@@ -13,7 +13,13 @@ import java.util.Set;
 @Repository
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
 
-//    @Query("SELECT * FROM location_entity WHERE category = ?1 AND tags CONTAINS ?2 AND ")
-    @Query(value = "SELECT * FROM location_entity", nativeQuery = true)
+    @Query(value = "SELECT * FROM location_entity WHERE category = ?1 AND " +
+            "(((time_monday_open <= ?3 AND time_monday_close >= ?4) OR (time_monday_open = 0 AND time_monday_close = 1440)) " +
+            "OR ((time_tuesday_open <= ?3 AND time_tuesday_close >= ?4) OR (time_tuesday_open = 0 AND time_tuesday_close = 1440)) " +
+            "OR ((time_wednesday_open <= ?3 AND time_wednesday_close >= ?4) OR (time_wednesday_open = 0 AND time_wednesday_close = 1440)) " +
+            "OR ((time_thursday_open <= ?3 AND time_thursday_close >= ?4) OR (time_thursday_open = 0 AND time_thursday_close = 1440)) " +
+            "OR ((time_friday_open <= ?3 AND time_friday_close >= ?4) OR (time_friday_open = 0 AND time_friday_close = 1440)) " +
+            "OR ((time_saturday_open <= ?3 AND time_saturday_close >= ?4) OR (time_saturday_open = 0 AND time_saturday_close = 1440)) " +
+            "OR ((time_sunday_open <= ?3 AND time_sunday_close >= ?4) OR (time_sunday_open = 0 AND time_sunday_close = 1440)))", nativeQuery = true)
     List<LocationEntity> getLocations(Category category, Set<TagEntity> tags, Integer timeFrom, Integer timeTo);
 }
