56f4f3717bcd1d112d862014f186350f17afd8c7
Brat-vseznamus <fvixnin@gmail.com>
2022-12-09 22:59:05 +0300

    fix 2

--- src/main/java/bot/app/abilities/StopPollAbility.java
+++ src/main/java/bot/app/abilities/StopPollAbility.java
@@ -45,9 +45,11 @@ public class StopPollAbility extends AbilityTemplate {
 
         pollService.stopPoll(userId);
         SendMessage sm = new SendMessage();
-        sm.setText("Спасибо за ответы, ожидайте когда сервис создаст мероприятие специального для вас, " +
+        sm.setText("*Спасибо за ответы*!\n " +
+                "Ожидайте когда сервис создаст мероприятие специального для вас, " +
                 "чтобы проверить готовность - нажмите `\\check`!");
         sm.setChatId(Long.toString(chatId));
+        sm.setParseMode("Markdown");
         try {
             tgbot.execute(sm);
         } catch (TelegramApiException e) {
