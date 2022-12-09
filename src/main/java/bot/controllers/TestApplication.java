60ac2d4f32f6197720ddc253721da96ba3b01c02
mk17ru <mk17.ru@mail.ru>
2022-12-09 20:11:46 +0300

    add fixes

--- src/main/java/bot/controllers/TestApplication.java
+++ src/main/java/bot/controllers/TestApplication.java
@@ -2,6 +2,7 @@ package bot.controllers;
 
 import bot.app.TelegramBot;
 import bot.app.utils.data.questions.GeneratedQuestionResult;
+import bot.backend.nodes.events.ActiveEvent;
 import bot.backend.nodes.events.CultureEvent;
 import bot.backend.nodes.events.Event;
 import bot.backend.nodes.events.FoodEvent;
@@ -36,7 +37,9 @@ public class TestApplication {
 
     @GetMapping("/testFoodAndCulture")
     public void testPredict() {
-        var result = predictService.generateTimeTable(List.of(
+        for (CultureEvent.CultureType cur : CultureEvent.CultureType.values()) {
+
+            var result = predictService.generateTimeTable(List.of(
                 new GeneratedQuestionResult(new TimeRestriction(new Event.Time(16 * 60, Integer.MAX_VALUE))),
                 new GeneratedQuestionResult(new TimeRestriction(new Event.Time(Integer.MIN_VALUE, 20 * 60))),
                 new GeneratedQuestionResult(new CountRestriction(3)),
@@ -44,9 +47,29 @@ public class TestApplication {
                 new GeneratedQuestionResult(new KitchenRestriction(List.of(FoodEvent.KitchenType.JAPANESE))),
                 new GeneratedQuestionResult(new BudgetRestriction(new Event.Budget(100, 1000))),
                 new GeneratedQuestionResult(new DateRestriction(LocalDate.now())),
-                new GeneratedQuestionResult(new CultureRestriction(Set.of(CultureEvent.CultureType.THEATRE)))
-        ));
-        System.out.println(result);
+                new GeneratedQuestionResult(new CultureRestriction(Set.of(cur)))
+            ));
+            System.out.println(result);
+            System.out.println("---------------------------");
+        }
+    }
+
+    @GetMapping("/testSport")
+    public void testActive() {
+        for (ActiveEvent.ActiveType cur : ActiveEvent.ActiveType.values()) {
+            var result = predictService.generateTimeTable(List.of(
+                    new GeneratedQuestionResult(new TimeRestriction(new Event.Time(16 * 60, Integer.MAX_VALUE))),
+                    new GeneratedQuestionResult(new TimeRestriction(new Event.Time(Integer.MIN_VALUE, 20 * 60))),
+                    new GeneratedQuestionResult(new CountRestriction(3)),
+                    new GeneratedQuestionResult(new FoodPlaceTypeRestriction(List.of(FoodEvent.FoodPlaceType.RESTAURANT))),
+                    new GeneratedQuestionResult(new KitchenRestriction(List.of(FoodEvent.KitchenType.JAPANESE))),
+                    new GeneratedQuestionResult(new BudgetRestriction(new Event.Budget(100, 1000))),
+                    new GeneratedQuestionResult(new DateRestriction(LocalDate.now())),
+                    new GeneratedQuestionResult(new ActiveRestriction(List.of(cur)))
+            ));
+            System.out.println(result);
+            System.out.println("---------------------------");
+        }
     }
 
     @GetMapping("/testOut")
